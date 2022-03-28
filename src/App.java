29680aedd48ab08f382500ab0fc2d990c0dcd9cc
Yeleuzhan <eleu397@gmail.com>
2022-03-28 13:49:11 +0600

    added main class

---
+++ src/App.java
@@ -0,0 +1,7 @@
+
+public class App {
+    public static void main(String[] args) {
+        Phone phone = new Smartphone("Samsung Galaxy5", 400, 2014, 16, Color.WHITE, 5.8);
+        System.out.println(phone);
+    }
+}
